049371f273f187cbc6726cab256e6bbdc5230301
Asher Novelli <asher.novelli@angelus.lan>
2023-06-01 15:52:03 -0300

    more exemples of variables

--- src/bin/variables.rs
+++ src/bin/variables.rs
@@ -7,7 +7,7 @@ fn main() {
 
     println!("This is a mutable variable! {:?}", variable_mutable);
 
-    // This is a type of repeating structure, have a more exemples in repeating.rs
+    // This is a type of repeating structure and decision, have a more exemples in repeating.rs decision_operators.rs
 
     loop {
         println!("Yes, I'am mutable {:?}", variable_mutable);
@@ -24,7 +24,40 @@ fn main() {
 
     println!("This is a integer number {:?}", number_integer);
 
-    let number_float : f32 = 3.1415926;
+    let number_float : f64 = 3.1415926;
 
     println!("This is a float number {:?}. 'Oh, I'm number PI'", number_float);
+
+
+    // In rust, there are only types f32 and f64, for int it has types i8, i16, i32, i64, i128
+
+    let boolean : bool = true;
+
+    println!("I'm a Bool, {:?}?", boolean);
+
+    let boolean : bool = false;
+
+    println!("No, you are not true, you are {:?}", boolean);
+
+    // ATTENTION, char in rust is declared using ''
+
+    let char_variable : char = 'A';
+
+    println!("I'm a char variable, {:?}", char_variable);
+
+    let array_type = [1,2,3,4,5];
+
+    println!("Array: {:?}", array_type);
+
+    // A array the lenght four and integer type
+
+    let array_type : [i32; 4] = [1,2,3,4];
+
+    println!("Array: {:?}", array_type);
+
+    // Array of one type element and lenght four
+
+    let array_type = [3; 5];
+
+    println!("Array of three's: {:?}", array_type);
 }
